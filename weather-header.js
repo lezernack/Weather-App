05e513b0d51d0f55fae47b9fdb3396b91046b2cc
lezernack <luks.ezck@gmail.com>
2024-02-26 14:56:48 -0700

    news section working

--- weather-header.js
+++ weather-header.js
@@ -1,4 +1,4 @@
-const url = `https://api.openweathermap.org/data/3.0/onecall?lat={40.559810}&lon={-112.361346}&limit={1}&appid={42b2bd6a477109afc00d28704bef90ae}`;
+const url = `https://api.openweathermap.org/data/3.0/onecall?lat=40.559810&lon=-112.361346&limit=1&appid=42b2bd6a477109afc00d28704bef90ae`;
 
 fetch(url)
   .then((response) => response.json())
@@ -6,12 +6,12 @@ fetch(url)
     document.getElementById("five-city").textContent = jsObject.name;
 
     document.getElementById("current-humid").textContent =
-      jsObject.main.humidity;
+      jsObject.base.main.humidity;
 
     document.getElementById("current-windSpeed").textContent =
       jsObject.wind.speed;
 
-    const descrip = jsObject.weather[1].description;
+    const descrip = jsObject.weather[0].description;
     document.getElementById("current-desc").textContent = descrip;
 
     let temp = (document.getElementById("current-temp").textContent =
